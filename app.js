82a007e5f2860a12007f7f1272d8295435030bf8
Pochet Cassandre <cassandre.Pochet@epitech.eu>
2021-08-04 20:31:22 +0200

    responsive

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 const questionsArrows = document.querySelectorAll('.question img')
 
-questionsArrows.forEach(x => {
-    x.addEventListener('click', function () {
-        const currentChoice = this.parentNode.parentNode.childNodes[3];
-        const currentChoiceHeight = this.parentNode.parentNode.childNodes[3].scrollHeight;
+questionsArrows.forEach((value) => {
+    value.addEventListener('click', () => {
+      value.parentNode.childNodes[3].classList.toggle('turnArrow');
+      value.parentNode.parentNode.childNodes[3].classList.toggle('open');
+    });
+  });
 
-        if (this.src.includes('down')) {
-            this.src = './images/icon-arrow-up.png';
-            this.parentNode.childNodes[1].style.color = 'black'
-            gsap.to(currentChoice, {
-                duration: 0.2, height: currentChoiceHeight + 40, opacity: 1,
-                padding: '20px 15px'
-            })
-        } else if (this.src.includes('up')) {
-            this.src = './images/icon-arrow-down.svg';
-            this.parentNode.childNodes[1].style.color = 'hsl(240, 6%, 50%)'
-            gsap.to(currentChoice, {
-                duration: 0.2, height: 0, opacity: 0,
-                padding: '0px 15px'
-            })
-        }
+const questionsTitle = document.querySelectorAll('.question h2')
 
-    })
-})
+questionsTitle.forEach((value) => {
+    value.addEventListener('click', () => {
+      value.parentNode.childNodes[3].classList.toggle('turnArrow');
+      value.parentNode.parentNode.childNodes[3].classList.toggle('open');
+    });
+  });
 
 const questionsAreas = document.querySelectorAll('.question h2')
 
@@ -33,5 +25,9 @@ questionsAreas.forEach(x => {
         
     x.addEventListener("mouseout", function() {
         x.style.color = "hsl(240, 6%, 50%)";
+    });
+
+    x.addEventListener("click", function() {
+        
     });
 })
